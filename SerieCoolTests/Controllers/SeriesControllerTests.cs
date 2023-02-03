d58fcc9ef1c983dc9a568c4a16e4b6e27c123097
Cessouille <celian.chausson@etu.univ-smb.fr>
2023-02-03 09:07:13 +0100

    Test GetSerie ok

--- SerieCoolTests/Controllers/SeriesControllerTests.cs
+++ SerieCoolTests/Controllers/SeriesControllerTests.cs
@@ -29,5 +29,20 @@ namespace SerieCool.Controllers.Tests
             Assert.IsInstanceOfType(result, typeof(IEnumerable<Serie>), "Pas un IEnumerable");
             CollectionAssert.AreEqual(lesSeries, result);
         }
+
+        [TestMethod()]
+        public void GetSerie_Ok()
+        {
+            // Arrange
+            var builder = new DbContextOptionsBuilder<SeriesDbContext>().UseNpgsql("Server=localhost;port=5432;Database=SeriesDB; uid=postgres; password=postgres;");
+            SeriesDbContext _context = new SeriesDbContext(builder.Options);
+            SeriesController seriesController = new SeriesController(_context);
+            // Act
+            var result = seriesController.GetSerie(140).Result.Value;
+            Serie serie = new Serie(140, "Mercredi", "Mercredi Addams est envoyée par ses parents, Gomez et Morticia, au sein de Nevermore Academy, à Jericho dans le Vermont, après avoir été une nouvelle fois expulsée d'un lycée.", 1, 8, 2022, "Netflix");
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(Serie), "Pas une Serie");
+            Assert.AreEqual(serie, result);
+        }
     }
 }
