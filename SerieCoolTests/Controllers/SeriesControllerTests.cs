0bbc59a4ab5be70009d168df0adbfd25de8f1d4d
Cessouille <celian.chausson@etu.univ-smb.fr>
2023-02-03 08:52:29 +0100

    Test GetSeries

--- SerieCoolTests/Controllers/SeriesControllerTests.cs
+++ SerieCoolTests/Controllers/SeriesControllerTests.cs
@@ -14,24 +14,20 @@ namespace SerieCool.Controllers.Tests
     [TestClass()]
     public class SeriesControllerTests
     {
-        private static SeriesController seriesController;
-
-        [ClassInitialize()]
-        public void SeriesControllerTest()
-        {
-            var builder = new DbContextOptionsBuilder<SeriesDbContext>().UseNpgsql("Server=localhost;port=5432;Database=SeriesDB; uid=postgres; password=postgres;");
-            SeriesDbContext _context = new SeriesDbContext(builder.Options);
-            seriesController = new SeriesController(_context);
-    }
-
         [TestMethod()]
-        public void GetSeriesTest()
+        public void GetSeriesTest_Ok()
         {
             // Arrange
+            var builder = new DbContextOptionsBuilder<SeriesDbContext>().UseNpgsql("Server=localhost;port=5432;Database=SeriesDB; uid=postgres; password=postgres;");
+            SeriesDbContext _context = new SeriesDbContext(builder.Options);
+            SeriesController seriesController = new SeriesController(_context);
+            List<Serie> lesSeries = new List<Serie>();
             // Act
-            var result = seriesController.GetSeries();
+            var result = seriesController.GetSeries().Result.Value.Where(s => s.Serieid == 140).ToList();
+            lesSeries.Add(new Serie(140,"Mercredi","Mercredi Addams est envoyée par ses parents, Gomez et Morticia, au sein de Nevermore Academy, à Jericho dans le Vermont, après avoir été une nouvelle fois expulsée d'un lycée.",1,8,2022,"Netflix"));
             // Assert
-            Assert.AreNotEqual(result, 0);
+            Assert.IsInstanceOfType(result, typeof(IEnumerable<Serie>), "Pas un IEnumerable");
+            CollectionAssert.AreEqual(lesSeries, result);
         }
     }
 }
