273e5377862a5967d2bbbd8cbad2fd884afb57ad
Cessouille <celian.chausson@etu.univ-smb.fr>
2023-02-07 16:11:04 +0100

    Modifications finales

---
+++ SerieCool/Models/SeriePart.cs
@@ -0,0 +1,42 @@
+﻿namespace SerieCool.Models.EntityFramework;
+
+public partial class Serie
+{
+    public Serie(int serieid, string titre, string? resume, int? nbsaisons, int? nbepisodes, int? anneecreation, string? network)
+    {
+        Serieid = serieid;
+        Titre = titre;
+        Resume = resume;
+        Nbsaisons = nbsaisons;
+        Nbepisodes = nbepisodes;
+        Anneecreation = anneecreation;
+        Network = network;
+    }
+
+    public Serie(string titre, string? resume, int? nbsaisons, int? nbepisodes, int? anneecreation, string? network)
+    {
+        Titre = titre;
+        Resume = resume;
+        Nbsaisons = nbsaisons;
+        Nbepisodes = nbepisodes;
+        Anneecreation = anneecreation;
+        Network = network;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Serie serie &&
+               Serieid == serie.Serieid &&
+               Titre == serie.Titre &&
+               Resume == serie.Resume &&
+               Nbsaisons == serie.Nbsaisons &&
+               Nbepisodes == serie.Nbepisodes &&
+               Anneecreation == serie.Anneecreation &&
+               Network == serie.Network;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Serieid, Titre, Resume, Nbsaisons, Nbepisodes, Anneecreation, Network);
+    }
+}
--- SerieCoolTests/Controllers/SeriesControllerTests.cs
+++ SerieCoolTests/Controllers/SeriesControllerTests.cs
@@ -23,9 +23,12 @@ namespace SerieCool.Controllers.Tests
             SeriesController seriesController = new SeriesController(_context);
 
             Serie serie = new Serie(100000, "Mercredi", "Mercredi Addams est envoyée par ses parents, Gomez et Morticia, au sein de Nevermore Academy, à Jericho dans le Vermont, après avoir été une nouvelle fois expulsée d'un lycée.", 1, 8, 2022, "Netflix");
-            var result = seriesController.GetSerie(140);
+            var result = seriesController.GetSerie(100000);
             if (result.Result.Value == null)
                 seriesController.PostSerie(serie);
+            result = seriesController.GetSerie(100000000);
+            if (result.Result.Value != null)
+                seriesController.DeleteSerie(100000000);
         }
         [TestMethod()]
         public void GetSeriesTest_Ok()
@@ -36,8 +39,10 @@ namespace SerieCool.Controllers.Tests
             SeriesController seriesController = new SeriesController(_context);
             List<Serie> lesSeries = new List<Serie>();
             // Act
-            var result = seriesController.GetSeries().Result.Value.Where(s => s.Serieid == 140).ToList();
-            lesSeries.Add(new Serie(140,"Mercredi","Mercredi Addams est envoyée par ses parents, Gomez et Morticia, au sein de Nevermore Academy, à Jericho dans le Vermont, après avoir été une nouvelle fois expulsée d'un lycée.",1,8,2022,"Netflix"));
+            var result = seriesController.GetSeries().Result.Value.Where(s => s.Serieid <= 3).ToList();
+            lesSeries.Add(new Serie(1,"Scrubs","J.D. est un jeune médecin qui débute sa carrière dans l'hôpital du Sacré-Coeur. Il vit avec son meilleur ami Turk, qui lui est chirurgien dans le même hôpital. Très vite, Turk tombe amoureux d'une infirmière Carla. Elliot entre dans la bande. C'est une étudiante en médecine quelque peu surprenante. Le service de médecine est dirigé par l'excentrique Docteur Cox alors que l'hôpital est géré par le diabolique Docteur Kelso. A cela viennent s'ajouter plein de personnages hors du commun : Todd le chirurgien obsédé, Ted l'avocat dépressif, le concierge qui trouve toujours un moyen d'embêter JD... Une belle galerie de personnage !",9,184,2001,"ABC (US)"));
+            lesSeries.Add(new Serie(2,"James May's 20th Century","The world in 1999 would have been unrecognisable to anyone from 1900. James May takes a look at some of the greatest developments of the 20th century, and reveals how they shaped the times we live in now.",1,6,2007,"BBC Two"));
+            lesSeries.Add(new Serie(3,"True Blood","Ayant trouvé un substitut pour se nourrir sans tuer (du sang synthétique), les vampires vivent désormais parmi les humains. Sookie, une serveuse capable de lire dans les esprits, tombe sous le charme de Bill, un mystérieux vampire. Une rencontre qui bouleverse la vie de la jeune femme...",7,81,2008,"HBO"));
             // Assert
             Assert.IsInstanceOfType(result, typeof(IEnumerable<Serie>), "Pas un IEnumerable");
             CollectionAssert.AreEqual(lesSeries, result);
@@ -51,8 +56,8 @@ namespace SerieCool.Controllers.Tests
             SeriesDbContext _context = new SeriesDbContext(builder.Options);
             SeriesController seriesController = new SeriesController(_context);
             // Act
-            var result = seriesController.GetSerie(140);
-            Serie serie = new Serie(140, "Mercredi", "Mercredi Addams est envoyée par ses parents, Gomez et Morticia, au sein de Nevermore Academy, à Jericho dans le Vermont, après avoir été une nouvelle fois expulsée d'un lycée.", 1, 8, 2022, "Netflix");
+            var result = seriesController.GetSerie(1);
+            Serie serie = new Serie(1, "Scrubs", "J.D. est un jeune médecin qui débute sa carrière dans l'hôpital du Sacré-Coeur. Il vit avec son meilleur ami Turk, qui lui est chirurgien dans le même hôpital. Très vite, Turk tombe amoureux d'une infirmière Carla. Elliot entre dans la bande. C'est une étudiante en médecine quelque peu surprenante. Le service de médecine est dirigé par l'excentrique Docteur Cox alors que l'hôpital est géré par le diabolique Docteur Kelso. A cela viennent s'ajouter plein de personnages hors du commun : Todd le chirurgien obsédé, Ted l'avocat dépressif, le concierge qui trouve toujours un moyen d'embêter JD... Une belle galerie de personnage !", 9, 184, 2001, "ABC (US)");
             // Assert
             Assert.IsInstanceOfType(result.Result, typeof(ActionResult<Serie>), "Pas un Task<ActionResult<Serie>>");
             Assert.IsInstanceOfType(result.Result.Value, typeof(Serie), "Pas une Serie");
@@ -110,14 +115,14 @@ namespace SerieCool.Controllers.Tests
             SeriesDbContext _context = new SeriesDbContext(builder.Options);
             SeriesController seriesController = new SeriesController(_context);
             // Act
-            Serie serie = new Serie(140, "Mercredi", "Mercredi Addams est envoyée par ses parents, Gomez et Morticia, au sein de Nevermore Academy, à Jericho dans le Vermont, après avoir été une nouvelle fois expulsée d'un lycée.", 1, 8, 2022, "Netflix");
+            Serie serie = new Serie(100000000, "Mercredi", "Mercredi Addams est envoyée par ses parents, Gomez et Morticia, au sein de Nevermore Academy, à Jericho dans le Vermont, après avoir été une nouvelle fois expulsée d'un lycée.", 1, 8, 2022, "Netflix");
             var result = seriesController.PostSerie(serie);
             CreatedAtActionResult routeResult = (CreatedAtActionResult)result.Result.Result;
             // Assert
             Assert.IsInstanceOfType(result.Result, typeof(ActionResult<Serie>), "Pas un ActionResult");
             Assert.IsInstanceOfType(result.Result.Result, typeof(CreatedAtActionResult), "Pas un CreatedAtRouteResult");
             Assert.AreEqual(routeResult.StatusCode, StatusCodes.Status201Created);
-            Assert.AreEqual(routeResult.Value, new Serie(140, "Mercredi", "Mercredi Addams est envoyée par ses parents, Gomez et Morticia, au sein de Nevermore Academy, à Jericho dans le Vermont, après avoir été une nouvelle fois expulsée d'un lycée.", 1, 8, 2022, "Netflix"));
+            Assert.AreEqual(routeResult.Value, new Serie(100000000, "Mercredi", "Mercredi Addams est envoyée par ses parents, Gomez et Morticia, au sein de Nevermore Academy, à Jericho dans le Vermont, après avoir été une nouvelle fois expulsée d'un lycée.", 1, 8, 2022, "Netflix"));
         }
 
         /*[TestMethod()]
@@ -145,7 +150,7 @@ namespace SerieCool.Controllers.Tests
             SeriesDbContext _context = new SeriesDbContext(builder.Options);
             SeriesController seriesController = new SeriesController(_context);
 
-            var result = seriesController.GetSerie(140);
+            var result = seriesController.GetSerie(100000);
             seriesController.DeleteSerie(100000);
         }
     }
